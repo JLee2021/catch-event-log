ac7bf890721e50f4fffbfa252d2d331f5554a2d0
Joshua Lee <77109426+JLee2021@users.noreply.github.com>
2022-07-04 15:10:30 -0400

    adding file structure

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// materialize ui controls
+
+document.addEventListener("DOMContentLoaded", function () {
+  // nav menu
+  const menus = document.querySelectorAll(".side-menu");
+  M.Sidenav.init(menus, { edge: "right" });
+  // add catch form
+  const forms = document.querySelectorAll(".side-form");
+  M.Sidenav.init(forms, { edge: "left" });
+});
+
+// service worker controls
